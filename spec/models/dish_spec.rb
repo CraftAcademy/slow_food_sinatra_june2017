1001f49409984f8d8681d616795484f211f6485d
Raoul DIFFOUO <diraulo@gmail.com>
2017-07-20 11:15:16 +0200

    group all model specs in a models directory

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -6,5 +6,4 @@ describe Dish do
   it { is_expected.to have_property :description }
   it { is_expected.to have_property :price }
   it { is_expected.to belong_to :order_item }
-  # it { is_expected.to have_many(:order).through(:order_item) }
 end
